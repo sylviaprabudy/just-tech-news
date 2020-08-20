b3a6c67af1dd7826d3b2cb711fb0eb0fd0374cde
Sylvia <sylviaprabudy@gmail.com>
2020-08-19 22:31:49 -0500

    change models/Comments.js to models/Comment.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
     console.log(req.session);
     Post.findAll({
@@ -41,15 +42,7 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-
-    res.render('login');
-});
-
+// get single post
 router.get('/post/:id', (req, res) => {
     Post.findOne({
         where: {
@@ -87,7 +80,10 @@ router.get('/post/:id', (req, res) => {
             const post = dbPostData.get({ plain: true });
 
             // pass data to template
-            res.render('single-post', { post });
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+              });
         })
         .catch(err => {
             console.log(err);
@@ -95,9 +91,13 @@ router.get('/post/:id', (req, res) => {
         });
 });
 
-res.render('single-post', {
-    post,
-    loggedIn: req.session.loggedIn
-});
-
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
 module.exports = router;
